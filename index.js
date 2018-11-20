c5d5ff443f8e8aa13f645b1b8bb450dd5ccd26f2
eniallator <eniallatormc@gmail.com>
2018-11-20 16:33:10 +0000

    Added boilerplate code

---
+++ index.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+// ctx.beginPath()
+// ctx.arc(posX + radius / 2, posY, radius, 0, 2 * Math.PI, false)
+// ctx.fill()
+
+function run() {
+    // requestAnimationFrame(run)
+}
+
+run()
