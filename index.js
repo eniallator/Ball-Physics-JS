283f1003d152cdef0d3c8c9cfd3be0b86cd76adc
eniallator <eniallatormc@gmail.com>
2018-12-06 12:37:47 +0000

    Added infinite loop prevention when spawning in too many balls and it not finding a valid place to spawn them

--- index.js
+++ index.js
@@ -6,21 +6,38 @@ const velMult = 5
 const velMin = 2
 const balls = []
 
+const maxSpawnAttempts = 100
+let spawnAttempts = 0
+
 for (let i = 0; i < 5; i++) {
+    let ball
     const radius = 30 + Math.random() * 50
+
     const vel = {
         x: velMult * Math.random() - velMult / 2,
         y: velMult * Math.random() - velMult / 2
     }
-    balls.push(
-        new Ball(
+    vel.x = vel.x + velMin * (vel.x / Math.abs(vel.x))
+    vel.y = vel.y + velMin * (vel.y / Math.abs(vel.y))
+
+    do {
+        if (spawnAttempts > maxSpawnAttempts) {
+            console.log('max spawn attempts reached')
+            break
+        }
+        spawnAttempts++
+
+        ball = new Ball(
             radius + Math.random() * (boundary.x - 2 * radius),
             radius + Math.random() * (boundary.y - 2 * radius),
             radius,
-            vel.x + velMin * (vel.x / Math.abs(vel.x)),
-            vel.y + velMin * (vel.y / Math.abs(vel.y))
+            vel.x,
+            vel.y
         )
-    )
+    } while (balls.some(other => ball.checkCollision(other)))
+
+    if (spawnAttempts > maxSpawnAttempts) break
+    balls.push(ball)
 }
 
 function run() {
